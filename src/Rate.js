db74eed4948a80b0b37a4ee005cb133c57a8e228
Piotr Nowak <piotr.nowak.25.02.1978@gmail.com>
2020-08-13 19:12:57 +0200

    CurrencyList renders a div with the right id

--- src/Rate.js
+++ src/Rate.js
@@ -3,3 +3,5 @@ import React from 'react';
 export const Rate = ({ rate }) => (
     <div>{rate.currency}</div>
 );
+
+export const CurrencyList = () => <div id="currencyList"></div>;
--- test/Rate.test.js
+++ test/Rate.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Rate } from '../src/Rate';
+import ReactDOM, { render } from 'react-dom';
+import { Rate, CurrencyList } from '../src/Rate';
 
 describe('Rate', () => {
     let container;
@@ -24,5 +24,21 @@ describe('Rate', () => {
 
         render(<Rate rate={rate} />, container);
         expect(container.textContent).toMatch('euro');
+    });
+});
+
+describe('CurrencyList', () => {
+    let container;
+
+    beforeEach(() => {
+        container = document.createElement('div');
+    });
+
+    const render = (component) => ReactDOM.render(component, container);
+
+    it('renders a div with the right id', () => {
+        render(<CurrencyList currencies={[]} />, container);
+
+        expect(container.querySelector('div#currencyList')).not.toBeNull();
     });
 });
