797fcb0f9b646db214b3647be0c09105e4e984e1
Piotr Nowak <piotr.nowak.25.02.1978@gmail.com>
2020-09-24 22:14:01 +0200

    Use webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,37 @@
+const path = require('path');
+
 module.exports = {
-  mode: "development",
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'main.js',
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  devtool: 'inline-source-map',
   module: {
-    rules: [{
-      test: /\.(js|jsx)$/,
-      exclude: /node-modules/,
-      loader: "babel-loader"
-    },
-    {
-      test: /\.scss$/,
-      use: [
-        {
-          loader: "style-loader",
-        },
-        {
-          loader: "css-loader",
-        },
-        {
-          loader: "sass-loader",
-        },
-      ],
-    },]
-  }
-}
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node-modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+    ],
+  },
+};
